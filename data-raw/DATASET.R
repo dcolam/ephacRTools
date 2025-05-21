f58357bdb16deec1006a77e610cc62f7987e485c
dcolam <26661245+dcolam@users.noreply.github.com>
2025-05-21 14:18:35 +0200

    updated data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,13 +1,11 @@
 ## code to prepare `DATASET` dataset goes here
 
 devtools::load_all()
-
 ## Human Adrenal Gland Dataset SE with imaging data
 l_files <- list.files(path = "data-raw/hAG/Ephys/" ,pattern = "*.xlsx$", recursive = TRUE, full.names = TRUE)
 se_hAG <- prepareSE(l_files)
 
-l_files <- list.files(path = "data-raw/hAG/Imaging/" ,pattern = "*.db$", recursive = TRUE, full.names = TRUE)
-df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "pa")
+l_filel_filel_files <- list.files(path = "data-raw/hAG/Imaging/" ,pattern = "*.db$", recursive = TRUE, full.names = TRUE)
 se_hAG <- mergeSEandImg(se_hAG, df_img)
 usethis::use_data(se_hAG, overwrite = TRUE)
 
@@ -16,7 +14,10 @@ usethis::use_data(se_hAG, overwrite = TRUE)
 l_files <- list.files(path = "data-raw/iNeurons/" ,pattern = "*.xlsx$", recursive = TRUE, full.names = TRUE)
 se_iN <- prepareSE(l_files)
 
+
 l_files <- list.files(path = "data-raw/iNeurons/" ,pattern = "*.db$", recursive = TRUE, full.names = TRUE)
-df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "pa")
-se_iN <- mergeSEandImg(se_iN, df_img)
+df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "coloc")
+
+se_iN <- mergeSEandImg(se_iN, df_img, tableType = "coloc")
 usethis::use_data(se_iN, overwrite = TRUE)
+
