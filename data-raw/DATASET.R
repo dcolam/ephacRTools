96c3c64551e845ee1a038047b5c47848db6e2621
dcolam <26661245+dcolam@users.noreply.github.com>
2025-05-21 18:24:25 +0200

    Added Primary Neuron dataset added coloc to all datasets

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,8 +5,12 @@ devtools::load_all()
 l_files <- list.files(path = "data-raw/hAG/Ephys/" ,pattern = "*.xlsx$", recursive = TRUE, full.names = TRUE)
 se_hAG <- prepareSE(l_files)
 
-l_filel_filel_files <- list.files(path = "data-raw/hAG/Imaging/" ,pattern = "*.db$", recursive = TRUE, full.names = TRUE)
-se_hAG <- mergeSEandImg(se_hAG, df_img)
+l_files <- list.files(path = "data-raw/hAG/Imaging/" ,pattern = "*.db$", recursive = TRUE, full.names = TRUE)
+df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "coloc")
+se_hAG <- mergeSEandImg(se_hAG, df_img, tableType = "coloc")
+df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "pa")
+se_hAG <- mergeSEandImg(se_hAG, df_img, tableType = "pa")
+colData(se_hAG)
 usethis::use_data(se_hAG, overwrite = TRUE)
 
 
@@ -17,7 +21,20 @@ se_iN <- prepareSE(l_files)
 
 l_files <- list.files(path = "data-raw/iNeurons/" ,pattern = "*.db$", recursive = TRUE, full.names = TRUE)
 df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "coloc")
-
 se_iN <- mergeSEandImg(se_iN, df_img, tableType = "coloc")
+df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "pa")
+se_iN <- mergeSEandImg(se_iN, df_img, tableType = "pa")
 usethis::use_data(se_iN, overwrite = TRUE)
+
+
+## Primary neurons dataset with imaging data
+l_files <- list.files(path = "data-raw/PrimaryNeurons/" ,pattern = "*.xlsx$", recursive = TRUE, full.names = TRUE)
+se_pn <- prepareSE(l_files)
+l_files <- list.files(path = "data-raw/PrimaryNeurons/" ,pattern = "*.db$", recursive = TRUE, full.names = TRUE)
+df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "coloc")
+se_pn <- mergeSEandImg(se_pn, df_img, tableType = "coloc")
+df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "pa")
+se_pn <- mergeSEandImg(se_pn, df_img, tableType = "pa")
+usethis::use_data(se_pn, overwrite = TRUE)
+
 
