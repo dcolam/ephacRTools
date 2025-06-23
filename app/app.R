3b9187cb8237f5d38766ce626bec50308bb8ac57
dcolam <26661245+dcolam@users.noreply.github.com>
2025-06-24 00:20:40 +0200

    password protection

--- app/app.R
+++ app/app.R
@@ -6,4 +6,15 @@ cat("Package loaded\n")
 #l_files <- list.files(path = "./" ,pattern = "*.xlsx$", recursive = TRUE, full.names = TRUE)
 #se_hAG <- ephacRTools::prepareSE(l_files)
 #print(se_hAG)
-ephacRTools::tinySEV(title = "ephacRTools")
+waiterContent <- tagList(
+  tags$div(
+    style = "text-align:center;",
+    tags$img(src = "https://raw.githubusercontent.com/dcolam/ephacRTools/master/inst/figures/ggplot2.svg", height = "100px", style = "margin-bottom: 20px;"),
+    waiter::spin_1(),
+    tags$h3("Please wait while the application is initialized...")
+  )
+)
+
+login <- data.frame(user="guest", password_hash="$7$C6..../....KBtKRNulXGoaOUSNltgKqkOukRedn06odEeU1CkM3X/$BrdNlK7/U1rVOnJKy2qRPqtKrWd3zyK6c009oZ68VVB")
+data("se_romk")
+ephacRTools::tinySEV(title = "ephacRTools",waiterContent =waiterContent, logins = login, objects = list("ROMK"= se_romk))
