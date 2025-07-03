b8d201a758c7cdcd34c20eb32ba0c617fd18fcc6
akschum <athenaschumacher@icloud.com>
2025-07-03 15:02:55 +0200

    ROMK data incorporation

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -21,3 +21,16 @@ df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "coloc")
 se_iN <- mergeSEandImg(se_iN, df_img, tableType = "coloc")
 usethis::use_data(se_iN, overwrite = TRUE)
 
+
+## ROMK
+
+l_files <- list.files(path = "data-raw/ROMK/" ,pattern = "*.xlsx$", recursive = TRUE, full.names = TRUE)
+se_romk <- prepareSE(l_files)
+
+l_files <- list.files(path = "data-raw/ROMK/" ,pattern = "*.db$", recursive = TRUE, full.names = TRUE)
+df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "coloc")
+se_romk <- mergeSEandImg(se_romk, df_img, tableType = "coloc")
+df_img <- prepareImgDF(l_files, scale_num = TRUE, analysis = "pa")
+
+se_romk <- mergeSEandImg(se_romk, df_img, tableType = "pa")
+usethis::use_data(se_romk, overwrite = TRUE)
